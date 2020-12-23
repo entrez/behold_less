59f09d0aef31361cb688718a463cfdcf2fe5ebd9
Michael Meyer <me@entrez.cc>
2020-12-23 18:01:34 -0500

    Set and load options through Weechat
    Instead of needing to edit the python script itself to set things like
    min_turns and min_points, you can set the values with weechat e.g.
    
        /set plugins.var.python.behold-less.always_show_users "qt"
    
    This will make it a lot easier to adjust options as desired.

--- behold-less.py
+++ behold-less.py
@@ -28,16 +28,13 @@ SCRIPT_VERSION = "0.0.1"
 SCRIPT_LICENSE = "GPL3"
 SCRIPT_DESC = "Hide Beholder and Rodney spam"
 
-# set MIN_TURN or MIN_POINTS to a negative number to disable that rule
-# (i.e. set MIN_TURN = -1, MIN_POINTS = 2000 to show events with at least 2000
-# points and ignore turn count)
-# of whether they
-MIN_TURN = 15000
-MIN_POINTS = 20000
-# events from users in this list will never be hidden
-ALWAYS_SHOW_USERS = ["qt"]
-# set BUFFER_NAME to "" in order to hide filtered messages entirely
-BUFFER_NAME = "behold-less"
+# set min_turn or min_points to "" to disable showing events fitting that rule
+# always_show_users: always show events from users in this comma-delimited list
+# buffer_name: set to "" in order to hide filtered messages entirely
+options = { "min_turn": "20000",
+            "min_points": "40000",
+            "always_show_users": "",
+            "buffer_name": "behold-less" }
 
 beholder_re = re.compile("\[[^\]]*\] \[.*[0-9](?P<variant>[A-Za-z]*)[^\]]*\] (?P<user>\S*) \((?P<class>\S*) (?P<race>\S*) (?P<gender>\S*) (?P<alignment>\S*)\)(?:, (?P<points>[0-9]*) points, T:(?P<endturn>[0-9]*), (?P<reason>.*)| (?P<event>.*), on T:(?P<eventturn>[0-9]*))")
 rodney_re = re.compile("(?P<user>\S*) \((?P<class>\S*) (?P<race>\S*) (?P<gender>\S*) (?P<alignment>\S*)\)(?:, (?P<points>[0-9]*) points, T:(?P<endturn>[0-9]*), (?P<reason>.*))")
@@ -45,12 +42,25 @@ wish_re = re.compile("(?:wished for|made (?:his|her|their) first(?: artifact)? w
 ascension_re = re.compile("ascended")
 
 
+def set_up_options():
+    for option, default_value in options.items():
+        if not weechat.config_is_set_plugin(option):
+            weechat.config_set_plugin(option, default_value)
+        else:
+            options[option] = weechat.config_get_plugin(option)
+
+
+def config_hook(data, option, value):
+    options[option] = value
+    return weechat.WEECHAT_RC_OK
+
+
 def make_buffer_if_needed():
-    if len(BUFFER_NAME) == 0:
+    if len(options["buffer_name"]) == 0:
         return ""
-    buffer = weechat.buffer_search("", BUFFER_NAME)
+    buffer = weechat.buffer_search("", options["buffer_name"])
     if buffer is None or buffer == "":
-        buffer = weechat.buffer_new(BUFFER_NAME, "", "", "", "")
+        buffer = weechat.buffer_new(options["buffer_name"], "", "", "", "")
     return buffer
 
 
@@ -59,28 +69,30 @@ def hardfought_hook(data, line):
     line_info = beholder_re.match(msg)
     # show unidentifiable messages by default
     if line_info is None:
-        return
+        return weechat.WEECHAT_RC_OK
     user = line_info.group("user")
-    if user in ALWAYS_SHOW_USERS:
-        return
+    if user in [user.strip() for user in
+                options["always_show_users"].split(",")]:
+        return weechat.WEECHAT_RC_OK
     if line_info.group("eventturn") is not None:
         turn = int(line_info.group("eventturn"))
         points = 0
         event = line_info.group("event")
         # if the event involves wishing, show it regardless of turn count
         if wish_re.match(event):
-            return
+            return weechat.WEECHAT_RC_OK
     else:
         turn = int(line_info.groupdict().get("endturn", 0))
         points = int(line_info.groupdict().get("points", 0))
         event = line_info.group("reason")
         # show all ascensions
         if ascension_re.search(event):
-            return
+            return weechat.WEECHAT_RC_OK
     # show late-game or high-point events and deaths
-    if (MIN_TURN >= 0 and turn >= MIN_TURN) \
-            or (MIN_POINTS >= 0 and points >= MIN_POINTS):
-        return
+    if (options["min_turn"] != "" and turn >= int(options["min_turn"])) \
+            or (options["min_points"] != ""
+                and points >= int(options["min_points"])):
+        return weechat.WEECHAT_RC_OK
     return {"buffer": make_buffer_if_needed(), "notify_level": "-1"}
 
 
@@ -89,26 +101,30 @@ def nethack_hook(data, line):
     line_info = rodney_re.match(msg)
     # show unidentifiable messages by default
     if line_info is None:
-        return
+        return weechat.WEECHAT_RC_OK
     user = line_info.group("user")
-    if user in ALWAYS_SHOW_USERS:
-        return
+    if user in [user.strip() for user in
+                options["always_show_users"].split(",")]:
+        return weechat.WEECHAT_RC_OK
     turn = int(line_info.groupdict().get("endturn", 0))
     points = int(line_info.groupdict().get("points", 0))
     event = line_info.group("reason")
     # show all ascensions
     if ascension_re.search(event):
-        return
+        return weechat.WEECHAT_RC_OK
     # show late-game deaths
-    if (MIN_TURN >= 0 and turn >= MIN_TURN) \
-            or (MIN_POINTS >= 0 and points >= MIN_POINTS):
-        return
+    if (options["min_turn"] != "" and turn >= int(options["min_turn"])) \
+            or (options["min_points"] != ""
+                and points >= int(options["min_points"])):
+        return weechat.WEECHAT_RC_OK
     return {"buffer": make_buffer_if_needed(), "notify_level": "-1"}
 
 
 if __name__ == '__main__' and import_ok and \
         weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION,
                          SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
+    set_up_options()
+    weechat.hook_config("plugins.var.python." + SCRIPT_NAME + ".*", "config_hook", "")
     hook = weechat.hook_line("", "*#hardfought", "nick_Beholder",
                              "hardfought_hook", "")
     hook = weechat.hook_line("", "*#NetHack", "nick_Rodney",
